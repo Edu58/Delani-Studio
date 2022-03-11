6b2e2c76a44e25afb0e8f4319d3b2ec0fe0e4528
Edu58 <edumuriithi58@gmail.com>
2022-03-11 14:15:36 +0300

    feat: toggles design div

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $().ready(function () {
     $('#design-desc, #dev-desc, #project-desc').hide()
+    $('#design-div').click(function () {
+        $('#design-img').toggle()
+        $('#design-desc').toggle()
+    })
 })
