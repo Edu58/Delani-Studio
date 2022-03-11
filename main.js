e89d873d6081ba73f40397149a2e745831a8acf5
Edu58 <edumuriithi58@gmail.com>
2022-03-11 19:30:06 +0300

    feat: toggles what we do images and descs

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-$().ready(function () {
+$(function () {
     $('#design-desc, #dev-desc, #project-desc').hide()
-    $('#design-div').click(function () {
-        $('#design-img').toggle()
-        $('#design-desc').toggle()
+
+    $('#design-div').on('click', function () {
+        $('#design-img').fadeToggle()
+        $('#design-desc').fadeToggle()
     })
+
+    $('#dev-div').on('click', function () {
+        $('#dev-img').fadeToggle()
+        $('#dev-desc').fadeToggle()
+    })
+
+    $('#project-div').on('click',function () {
+        $('#project-img').fadeToggle()
+        $('#project-desc').fadeToggle()
+    })
+
+
 })
